94cef755843a59cbe657bd7c5f9a4b1d3f250f21
Matas Bartosevicius <bartosem@aston.ac.uk>
2017-12-04 18:21:08 +0000

    stop search class(needs work)

---
+++ MTRSystem/src/mtr/StopSearch.java
@@ -0,0 +1,13 @@
+package mtr;
+
+public class StopSearch {
+	private Station currentStation;
+	private Station destination;
+	
+		public StopSearch(Station c, Station d)
+		{
+			currentStation = c;
+			destination = d;
+			
+		}
+}
