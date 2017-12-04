a730a9768eb3b4d284c0076309949bca2815ba63
ollie <ollie@10.79.11.66>
2017-12-04 18:02:17 +0000

    Add stop and line fields to class, redefine constructor from standard method

--- MTRSystem/src/mtr/MTRSystem.java
+++ MTRSystem/src/mtr/MTRSystem.java
@@ -1,5 +1,8 @@
 package mtr;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * @author Ollie
  * @version 1.0
@@ -14,7 +17,18 @@ public class MTRSystem {
 	//create lists of lines or stations
 	//add lines and stations
 	
-	public void MTRSystem() {
+	/*
+	 * Hashmap containing lines, indexed by their name, representing lines as a linked list
+	 */
+	private HashMap<String, DoublyLinkedList> lines = new HashMap<>();
+	
+	/*
+	 * Arraylist containing a list of unique stops in this system, for ensuring linear time operations
+	 */
+	private ArrayList<Stop> stops = new ArrayList<>();
+	
+	
+	public MTRSystem() {
 		
 	}
 	
